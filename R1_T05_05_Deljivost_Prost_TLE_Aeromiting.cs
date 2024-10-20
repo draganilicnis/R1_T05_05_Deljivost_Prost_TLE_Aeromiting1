84d08b450db7da532bc9237edc3da9676e949d2a
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2024-10-20 16:17:49 +0200

    Update R1_T05_05_Deljivost_Prost_TLE_Aeromiting.cs

--- R1_T05_05_Deljivost_Prost_TLE_Aeromiting.cs
+++ R1_T05_05_Deljivost_Prost_TLE_Aeromiting.cs
@@ -1,9 +1,33 @@
+// https://petlja.org/sr-Latn-RS/biblioteka/r/Zbirka3/aeromiting
+// https://petlja.org/sr-Latn-RS/kurs/14606/23/2756
+// https://github.com/draganilicnis/R1_T05_05_Deljivost_Prost_TLE_Aeromiting1/blob/main/R1_T05_05_Deljivost_Prost_TLE_Aeromiting.cs
+
 using System;
 
 class R1_T05_05_Deljivost_Prost_TLE_Aeromiting
 {
+    static long Dopuna_do_punog_kvadrata(long n)
+    {
+        long m = 1;
+        long d = 2;
+        while (d * d <= n)
+        {
+            int k = 0;
+            while (n % d == 0)
+            {
+                n = n / d;
+                k++;
+            }
+            if (k % 2 != 0) m = m * d;
+            d++;
+        }
+        if (n > 1) m = m * n;
+        return m;
+    }
     static void Main()
     {
-        Console.WriteLine("Hello World!");
+        long n = long.Parse(Console.ReadLine());
+        long m = Dopuna_do_punog_kvadrata(n);
+        Console.WriteLine(n/m);
     }
 }
